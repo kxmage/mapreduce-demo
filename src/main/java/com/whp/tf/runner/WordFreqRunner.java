32d3659fb3683e3fbba04cf8fc2c76aebd6d0653
ia-wang_hp <ia-wang_hp@iap-wang_hp>
2017-02-06 14:15:54 +0800

    1

--- src/main/java/com/whp/tf/runner/WordFreqRunner.java
+++
@@ -1,9 +0,0 @@
-package com.whp.tf.runner;
-
-import org.apache.hadoop.util.ToolRunner;
-
-public class WordFreqRunner{
-	public static void main(String[] args) throws Exception {
-		System.exit(ToolRunner.run(new WordFreqMSNewsRunner(), args));
-	}
-}
